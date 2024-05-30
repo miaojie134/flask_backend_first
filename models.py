c5c003a07db6e0a329e12bd91e85e2f17abd0455
MJ <miaojie134@foxmail.com>
2024-05-30 17:02:24 +0800

    first push

---
+++ models.py
@@ -0,0 +1,63 @@
+from exts import db
+
+"""
+class Recipe:
+    id: integer
+    title: string
+    description: string
+"""
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<Recipe {self.title}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, title, description):
+        self.title = title
+        self.description = description
+        db.session.commit()
+
+
+"""
+class User:
+    id: integer
+    username: string
+    email: string
+    password: string
+"""
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+        db.session.commit()
